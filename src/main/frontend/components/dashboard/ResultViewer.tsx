17e425914608f7c213c9a6c4533fcb9fac258059
Simon Hiller <simon.hiller@glowingblue.com>
2024-08-21 13:25:56 +0200

    chor: add some data to result viewer

--- src/main/frontend/components/dashboard/ResultViewer.tsx
+++ src/main/frontend/components/dashboard/ResultViewer.tsx
@@ -38,8 +38,19 @@ export default function ResultViewer({testResult}: ResultViewerProps): React.JSX
         }
     }, [selectedFile]);
 
-    let chart = null;
-    if(dataPoints[0].length > 0) {
+    let chart, info = null;
+    if (dataPoints[0].length > 0) {
+        const maxForce = Math.round(Math.max(...dataPoints[1])) / 1000;
+        const minForce = Math.round(Math.min(...dataPoints[1])) / 1000;
+        const duration = dataPoints[0][dataPoints[0].length - 1] - dataPoints[0][0];
+        info = (
+            <VerticalLayout theme="padding spacing-l stretch evenly">
+                <h3 style={{width: '8em'}}>Duration: {duration} kN</h3>
+                <h3 style={{width: '8em'}}>Max Force: {maxForce} kN</h3>
+                <h3 style={{width: '8em'}}>Min Force: {minForce} kN</h3>
+            </VerticalLayout>
+        )
+
         chart = (<Plot
             data={[
                 {
@@ -73,19 +84,18 @@ export default function ResultViewer({testResult}: ResultViewerProps): React.JSX
             }}/>);
     }
 
-    const items = dataFiles.map((file) => ({label: file, value: file, disabled:false}));
+    const items = dataFiles.map((file) => ({label: file, value: file, disabled: false}));
     items.unshift({label: 'Select file', value: "", disabled: true});
 
     let testparamInfo = null;
-    if(testResult.testParameter.type === 'cyclic') {
+    if (testResult.testParameter.type === 'cyclic') {
         testparamInfo = (
             <ul>
                 <li>Upper turn force: {testResult.testParameter.upperTurnForce}</li>
                 <li>Lower turn force: {testResult.testParameter.lowerTurnForce}</li>
             </ul>
         );
-    }
-    else {
+    } else {
         testparamInfo = (
             <ul>
                 <li>Upper shut off threshold: {testResult.testParameter.upperShutOffThreshold}</li>
@@ -106,8 +116,9 @@ export default function ResultViewer({testResult}: ResultViewerProps): React.JSX
             </dl>
             <HorizontalLayout theme="padding spacing-l stretch evenly">
                 <label>Select data file:</label>
-                <Select onChange={(e) => setSelectedFile(e.target.value)} items={items} value={selectedFile} />
+                <Select onChange={(e) => setSelectedFile(e.target.value)} items={items} value={selectedFile}/>
             </HorizontalLayout>
+            {info}
             {chart}
         </VerticalLayout>
     );
