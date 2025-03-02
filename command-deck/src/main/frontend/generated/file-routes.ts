bdec347d3daa21a2885d34aff1cc756da49eab54
Simon Hiller <simon.hiller@glowingblue.com>
2025-03-02 22:44:55 +0100

    feat: implement docker setup

--- command-deck/src/main/frontend/generated/file-routes.ts
+++ command-deck/src/main/frontend/generated/file-routes.ts
@@ -3,20 +3,18 @@ import type { AgnosticRoute as AgnosticRoute_1 } from "@vaadin/hilla-file-router
 import * as Page_1 from "../views/@index.js";
 import * as Layout_1 from "../views/@layout.js";
 import * as Page_2 from "../views/control.js";
-import * as Page_3 from "../views/login.js";
-import * as Page_4 from "../views/result/{resultId}/tracking.js";
-import * as Page_5 from "../views/run.js";
+import * as Page_3 from "../views/result/{resultId}/tracking.js";
+import * as Page_4 from "../views/run.js";
 const routes: readonly AgnosticRoute_1[] = [
     createRoute_1("", Layout_1, [
         createRoute_1("", Page_1),
         createRoute_1("control", Page_2),
-        createRoute_1("login", Page_3),
         createRoute_1("result", [
             createRoute_1(":resultId", [
-                createRoute_1("tracking", Page_4)
+                createRoute_1("tracking", Page_3)
             ])
         ]),
-        createRoute_1("run", Page_5)
+        createRoute_1("run", Page_4)
     ])
 ];
 export default routes;
---
+++ command-deck/src/main/frontend/routes.tsx
@@ -0,0 +1,37 @@
+import {RouterConfigurationBuilder} from "@vaadin/hilla-file-router/runtime.js";
+import fileRoutes from "Frontend/generated/file-routes";
+import cmsFileRoutes from "cms/generated/file-routes";
+import Flow from "Frontend/generated/flow/Flow";
+
+export const { router, routes } = new RouterConfigurationBuilder()
+    .withFileRoutes(cmsFileRoutes)
+    //.withFileRoutes(fileRoutes) // (1)
+    // To define routes manually or adding an individual route, use the
+    // following code and remove (1):
+    // .withReactRoutes(
+    //     [
+    //       {
+    //         element: <MainLayout />,
+    //         handle: { title: 'Main' },
+    //         children: [
+    //             { path: '/hilla', element: <HillaView />, handle: { title: 'Hilla' } }
+    //         ],
+    //       },
+    //       { path: '/login', element: <Login />, handle: { title: 'Login' } }
+    //     ]
+    // )
+    // OR
+    // .withReactRoutes(
+    //   [
+    //     { path: '/login', element: <Login />, handle: { title: 'Login' } }
+    //   ]
+    // )
+    // To use Flow RouterLayout with hilla views use withLayout and define
+    // flowLayout as true in ViewConfig for view.
+    // .withLayout(Flow)
+    .withFallback(Flow)
+    // Optional method that adds an authentication for routes.
+    // Can take an optional path to redirect to, if not authenticated:
+    // .protect('/login');
+    .protect()
+    .build();
